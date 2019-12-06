2d4fd4831e39776ab95f012c777ce1d4fede7950
Konstadinos Angelis <angeliskonstadinos@gmail.com>
2019-12-06 09:39:38 -0800

    finished MVP / cards component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,43 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function createArticles(array){
+    const mainCard = document.createElement('div'),
+    headline = document.createElement('div'),
+    author = document.createElement('div'),
+    imgContainer = document.createElement('div'),
+    authorImg =  document.createElement('img')
+    authorName = document.createElement('span');
+
+    mainCard.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    mainCard.appendChild(headline);
+    mainCard.appendChild(author);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(authorImg);
+    imgContainer.appendChild(authorName);
+
+    headline.textContent = array.headline;
+    authorName.textContent = array.authorName;
+    authorImg.src = array.authorPhoto;
+    authorName.textContent = array.authorName;
+
+    return mainCard;
+}
+
+const articlePlacer = document.querySelector('.cards-container')
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+.then(response => {
+    let test = Object.values(response.data.articles);
+    test.forEach((entry) => {
+        entry.forEach(element => {
+            console.log(element)
+            articlePlacer.appendChild(createArticles(element))
+        })
+    })
+})
